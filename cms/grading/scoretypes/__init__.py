66b1ef9f36ca3ed43b4e6dae3cba69ead1e38032
Luca Wehrstedt <luca.wehrstedt@gmail.com>
2013-03-28 00:38:39 +0100

    grading: get_score_type

--- cms/grading/scoretypes/__init__.py
+++ cms/grading/scoretypes/__init__.py
@@ -24,33 +24,36 @@ import simplejson as json
 from cms import logger, plugin_lookup
 
 
-def get_score_type(submission=None, task=None):
-    """Given a task, instantiate the corresponding ScoreType class.
+def get_score_type(dataset):
+    """Given a dataset, instantiate the corresponding ScoreType class.
 
-    submission (Submission): the submission that needs the task type.
-    task (Task): the task we want to score.
+    datset (Dataset): the Dataset whose ScoreType we want
 
     return (object): an instance of the correct ScoreType class.
 
     """
-    # Validate arguments.
-    if [x is not None
-        for x in [submission, task]].count(True) != 1:
-        raise ValueError("Need at most one way to get the score type.")
+    score_type_name = dataset.score_type
 
-    if submission is not None:
-        task = submission.task
-
-    score_type_name = task.score_type
     try:
-        score_type_parameters = json.loads(task.score_type_parameters)
+        score_type_parameters = json.loads(dataset.score_type_parameters)
     except json.decoder.JSONDecodeError as error:
-        logger.error("Cannot decode score type parameters.\n%r." % error)
-        raise
+        logger.error("Cannot decode score type parameters for task "
+            "%d \"%s\", dataset %d \"%s\"\n%r." % (
+                task.id, task.name, dataset.id, dataset.description,
+                error))
+        return None
+
     public_testcases = dict((testcase.num, testcase.public)
-                            for testcase in task.testcases)
+                            for testcase in dataset.testcases)
 
     cls = plugin_lookup(score_type_name,
                         "cms.grading.scoretypes", "scoretypes")
 
-    return cls(score_type_parameters, public_testcases)
+    try:
+        return cls(score_type_parameters, public_testcases)
+    except Exception as error:
+        logger.error("Cannot instantiate score type for task "
+            "%d \"%s\", dataset %d \"%s\"\n%r." % (
+                task.id, task.name, dataset.id, dataset.description,
+                error))
+        return None
